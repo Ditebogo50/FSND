d6efc5982c9b7c53263ffb4787feaf1bad1524a4
Suzan <sue7led@gmail.com>
2022-08-13 18:11:21 +0200

    seperated the models from app.py

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -18,6 +18,7 @@ from forms import *
 from flask_migrate import Migrate #import migrate
 import collections
 collections.Callable = collections.abc.Callable
+from models import db, Artist, Show, Venue
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -34,62 +35,62 @@ migrate = Migrate(app, db) #link migrate to our flask app
 # Models.
 #----------------------------------------------------------------------------#
 
-#association table with extra data
-class Show(db.Model):
-   __tablename__ = 'show'
-   venue_id = db.Column('venue_id', db.Integer, db.ForeignKey('venue.id'), primary_key=True)
-   artist_id = db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'), primary_key=True)
-   start_time = db.Column(db.DateTime, nullable=False)
+# #association table with extra data
+# class Show(db.Model):
+#    __tablename__ = 'show'
+#    venue_id = db.Column('venue_id', db.Integer, db.ForeignKey('venue.id'), primary_key=True)
+#    artist_id = db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'), primary_key=True)
+#    start_time = db.Column(db.DateTime, nullable=False)
 
 
-class Venue(db.Model):
-    __tablename__ = 'venue'
+# class Venue(db.Model):
+#     __tablename__ = 'venue'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     city = db.Column(db.String(120))
+#     state = db.Column(db.String(120))
+#     address = db.Column(db.String(120))
+#     image_link = db.Column(db.String(500))
+#     facebook_link = db.Column(db.String(120))
 
-    #added columns/fields
-    #phone
-    phone = db.Column(db.String(120))
-    #genres
-    genres = db.Column(db.String)
-    #website_link
-    website_link = db.Column(db.String())
-    #seeking_talent
-    seeking_talent = db.Column(db.Boolean)
-    #seeking_description
-    seeking_description = db.Column(db.String())
+#     #added columns/fields
+#     #phone
+#     phone = db.Column(db.String(120))
+#     #genres
+#     genres = db.Column(db.String)
+#     #website_link
+#     website_link = db.Column(db.String())
+#     #seeking_talent
+#     seeking_talent = db.Column(db.Boolean)
+#     #seeking_description
+#     seeking_description = db.Column(db.String())
 
-    #model relationships and properties
-    artists = db.relationship('Artist', secondary='show', backref=db.backref('venues', lazy=True))
+#     #model relationships and properties
+#     artists = db.relationship('Artist', secondary='show', backref=db.backref('venues', lazy=True))
 
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+#     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
-class Artist(db.Model):
-    __tablename__ = 'artist'
+# class Artist(db.Model):
+#     __tablename__ = 'artist'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     city = db.Column(db.String(120))
+#     state = db.Column(db.String(120))
+#     phone = db.Column(db.String(120))
+#     genres = db.Column(db.String(120))
+#     image_link = db.Column(db.String(500))
+#     facebook_link = db.Column(db.String(120))
 
-    #added columns/fields
-    #website_link
-    website_link = db.Column(db.String())
-    #seeking_venue
-    seeking_venue = db.Column(db.Boolean)
-    #seeking_description
-    seeking_description = db.Column(db.String())
+#     #added columns/fields
+#     #website_link
+#     website_link = db.Column(db.String())
+#     #seeking_venue
+#     seeking_venue = db.Column(db.Boolean)
+#     #seeking_description
+#     seeking_description = db.Column(db.String())
 
 
 
